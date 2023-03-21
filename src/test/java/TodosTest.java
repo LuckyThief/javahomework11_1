731e21215b5d4c9674db7216a372b99549e8ee3d
LuckyThief <luckythief13@gmail.com>
2023-03-21 09:41:37 +0400

    Tests added

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -102,4 +102,54 @@ public class TodosTest {
         Task[] actual = todos.search("Выкатка 3й версии приложения");
         Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void shouldFindZeroTasks() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = { };
+        Task[] actual = todos.search("Сварить компот");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindSomeTasks() {
+        SimpleTask simpleTask = new SimpleTask(5, "Хлеб");
+
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = { simpleTask, epic };
+        Task[] actual = todos.search("Хлеб");
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
